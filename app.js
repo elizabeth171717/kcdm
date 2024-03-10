f051fe2d050fa629677de4be0f32594f14257829
Elizabeth Teresa <69443070+elizabeth171717@users.noreply.github.com>
2024-03-10 15:30:53 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,119 +1,52 @@
 //gsap animation
-gsap.to(".image",{ scale:1, opacity:1, duration:2});
+gsap.from(".animated-img", { duration:1,scale:1.5});
+
+//firt scrolltrigger
+let scrollTL = gsap.timeline({
+  scrollTrigger: {
+    trigger:".animated-img2",
+    toggleActions:"play pause resume reverse"
+  }
+});
+scrollTL.from(".animated-img2", {opacity: 0, duration:1, scale: 0});
+
+//second scrolltrigger
+
+let scrollTL2 = gsap.timeline({
+  scrollTrigger: {
+    trigger:".card",
+    toggleActions:"play pause resume reverse"
+  }
+});
+scrollTL2.from(".card", { scale: 0, stagger: {amount:0.5, from:"random"}, duration:1});
+
+//third scrolltrigger
+
+let scrollTL3 = gsap.timeline({
+  scrollTrigger: {
+    trigger:".move-up",
+    toggleActions:"play pause resume reverse"
+  }
+});
+scrollTL3.from(".move-up", { opacity: 0, y:"6rem", duration:1});
 
 
+// Get the button
+let mybutton = document.getElementById("myBtn");
 
-//NAVIGATION BAR STARTS
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar-menu');
-const navLogo = document.querySelector('#navbar-logo');
-
-//display mobile menu
-const mobileMenu = () => {
-    menu.classList.toggle('is-active')
-    menuLinks.classList.toggle('active')
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
 
-menu .addEventListener('click', mobileMenu);
-
-//show active menu when scrolling 
-
-const highlightMenu = () => {
-    const elem = document.querySelector('.highlight')
-    const homeMenu = document.querySelector('#home-page')
-    const aboutMenu = document.querySelector('#about-page')
-    const servicesMenu = document.querySelector('#services-page')
-    const contactMenu = document.querySelector('#contact-page')
-    let scrollPos = window.scrollY
-
-    //adds highlights class to my menu items
-
-    if (window.innerWidth > 960 && scrollPos < 600) {
-        homeMenu.classList.add('highlight')
-        aboutMenu.classList.remove('highlight')
-        return
-    } else if (window.innerWidth > 960 && scrollPos < 1400) {
-       
-        aboutMenu.classList.add('highlight')
-        homeMenu.classList.remove('highlight')
-        servicesMenu.classList.remove('highlight')
-        return
-    }   else if (window.innerWidth > 960 && scrollPos < 2345) {
-        servicesMenu.classList.add('highlight')
-        aboutMenu.classList.remove('highlight')
-       return
-    }
-
-    if (elem && window.innerWidth < 960 && scrollPos < 600 || elem) {
-        elem.classList.remove('highlight')
-    }
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
-
-window.addEventListener('scroll', highlightMenu);
-window.addEventListener('click', highlightMenu);
-
-//close mobile menu when clicking on a menu item
-
-
-const hideMobileMenu = () => {
-    const menuBars = document.querySelector('.is-active')
-    if(window.innerWidth <= 768 && menuBars) {
-        menu.classList.toggle('is-active')
-        menuLinks.classList.remove('active')
-    }
-}
-
-menuLinks.addEventListener('click', hideMobileMenu);
-navLogo.addEventListener('click', hideMobileMenu);
-
-//NAVIGATION BAR ENDS
-
-
-//gsap animation
-
-gsap.registerPlugin(ScrollTrigger);
-
-
-ScrollTrigger.defaults({
-    toggleActions: "restart pause resume pause"
-  });
-  
-  gsap.to(".home-container", {
-    scrollTrigger: ".home-container", 
-    duration: 1, 
-    rotation: 360
-  });
-
-
-  gsap.to(".animated-text", {
-    scrollTrigger: ".animated-text", 
-    duration: 1, 
-    rotation: 360
-  });
-
- gsap.to(".animated-img", {
-    scrollTrigger: ".animated-img", 
-    duration: 1, 
-    rotation: 360
-  });
-  
-gsap.to(".gallery-section", {
-    scrollTrigger: {
-      trigger: ".gallery-section",
-      toggleActions: "restart pause reverse pause"
-    }, 
-    duration: 2, 
-    backgroundColor: " hsl(210, 22%, 49%)", 
-    ease: "none"
-  });
-
- gsap.to(".service", {
-    scrollTrigger: {
-      trigger: ".service",
-      toggleActions: "restart pause reverse pause"
-    }, 
-    duration: 2, 
-    color: "#fff", 
-    ease: "none"
-  });
